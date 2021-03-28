cb16ad89748d1433e8e9c3118000bd23c2db7370
Chase <kristen880319@gmail.com>
2021-03-28 13:34:57 +0800

    Initial Commit

---
+++ SpellingGame/SpellingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingGameApp.swift
+//  SpellingGame
+//
+//  Created by Chase on 2021/3/28.
+//
+
+import SwiftUI
+
+@main
+struct SpellingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
